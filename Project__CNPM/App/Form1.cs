d3fd8d1441e9bd572e01c81d79e014d607511990
VolLove <103765155+VolLove@users.noreply.github.com>
2022-11-17 09:36:15 +0700

    update 9:36/17/11/2022

--- Project__CNPM/App/Form1.cs
+++ Project__CNPM/App/Form1.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace AppBuuDien
+namespace App
 {
     public partial class Form1 : Form
     {
+        string sqlcon = @"Data Source=DESKTOP-2PT5DI6;Initial Catalog=PROJECT;Integrated Security=True";
+        SqlConnection sqlConnection= null;
         public Form1()
         {
             InitializeComponent();
         }
-
     }
 }
